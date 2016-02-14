e1a9412c2cc6e1c871e3fc206695d8369b42c4a4
Frederick Criscuolo <fcriscuolo@nygenome.org>
2016-02-14 18:02:13 -0500

    add java files

--- src/main/java/org/nygenome/fcriscuolo/javaslang/dev/TestClass.java
+++ src/main/java/org/nygenome/fcriscuolo/javaslang/dev/TestClass.java
@@ -4,7 +4,7 @@ package org.nygenome.fcriscuolo.javaslang.dev;
  * Created by fcriscuolo on 12/30/15.
  */
 public class TestClass {
-    public static int sum(int first, int second) {
+    public static int sum(int first, int second) { 
         if (first >= 0 && second >= 0) {
             return first + second;
         } else {
